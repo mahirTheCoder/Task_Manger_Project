42c98003ef3bf908a3d9ac65cb314327a1d98d6c
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-04-17 19:57:06 +0600

    server connected

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("server js connected ");
+const express = require('express')
+const app = express()
+const router = require('./router')
+app.use(express.json());
+app.use(router)
+
+
+
+
+app.listen(8000, ()=>{
+    console.log('Server is running on port 8000')
+})
